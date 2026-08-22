e932bf84690243d0777ebf9dc9f28e802248f62a
Riadul Islam <riadul551@gmail.com>
2026-08-23 00:00:13 +0600

    add Navbar component and integrate it into layout; update favicon and logo assets

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Navbar from "@/components/shared/navbar";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="">
+      {/* <Navbar></Navbar> */}
       <h1>Rentora</h1>
       <Button>Sign in</Button>
     </div>
---
+++ src/components/shared/navbar.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import React from "react";
+import rentoraLogo from "../../app/assets/logo.png";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Navbar = () => {
+  return (
+    <section className="py-5 px-2  sticky shadow">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <Image src={rentoraLogo} alt="rentora logo" width={40} height={40} />
+        </div>
+        <div className="flex gap-2">
+          <Link href={"#"} className="p-2 cursor-pointer">
+            Home
+          </Link>
+          <Link href={"#"} className="p-2 cursor-pointer">
+            All Properties
+          </Link>
+        </div>
+        <div>
+          <Button>Log in</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
